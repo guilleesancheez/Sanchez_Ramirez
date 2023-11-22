fab48a6f9f7f6029797e00134c552105058ed33c
Guille <guillermo1999.gsr@gmail.com>
2023-11-22 10:00:46 +0100

    añadido método altaAlumno

---
+++ src/AlumnoCRUD.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class AlumnoCRUD {
+
+    private File f;
+
+    public AlumnoCRUD(String nombreFichero) {
+        try {
+            f = new File(nombreFichero);
+            f.createNewFile();
+            if (f.length() == 0) {
+                // Si el fichero indicado está vacío (ocupa 0 bytes), escribe un ArrayList vacío
+                FileOutputStream fos = new FileOutputStream(f);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                oos.writeObject(new ArrayList<Alumno>());
+                oos.flush();
+                oos.close();
+                fos.close();
+            }
+        }catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    /*
+        Escribe en el fichero un ArrayList de alumnos
+     */
+    public void escribirAlumnos(ArrayList<Alumno> listaAlumnos) {
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(listaAlumnos);
+            oos.flush();
+            oos.close();
+            fos.close();
+        }catch (IOException e) {
+            System.out.println("Fallo al escribir");
+        }
+    }
+
+    /*
+        Devuelve un ArrayList con todos los alumnos existentes en el fichero
+     */
+    public ArrayList<Alumno> leerAlumnos() {
+        ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+        try {
+            FileInputStream fis = new FileInputStream(f);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            listaAlumnos = (ArrayList<Alumno>) ois.readObject();
+            ois.close();
+            fis.close();
+        }catch (IOException | ClassNotFoundException e) {
+            System.out.println("Fallo al leer");
+        }
+        return listaAlumnos;
+    }
+
+    public void altaAlumno(Alumno alumno) {
+ArrayList<Alumno> listaAlumno = leerAlumnos();
+listaAlumno.add(alumno);
+escribirAlumnos(listaAlumno);
+}
+public void bajaAlumno(String dni) {
+ArrayList<Alumno> listaAlumno = leerAlumnos();
+for (int i = 0; i < listaAlumno.size(); i++)
+if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni))
+listaAlumno.remove(i);
+escribirAlumnos(listaAlumno);
+}
+
+public void actualizarAlumno(Alumno alumno) {
+ArrayList<Alumno> listaAlumnos = leerAlumnos();
+for (int i = 0; i < listaAlumnos.size(); i++) {
+if (listaAlumnos.get(i).getDni()
+.equalsIgnoreCase(alumno.getDni())) {
+listaAlumnos.set(i, alumno);
+}
+}
+escribirAlumnos(listaAlumnos);
+}
+public void mostrarTodos() {
+ArrayList<Alumno> listaAlumnos = leerAlumnos();
+for (Alumno a : listaAlumnos) System.out.println(a.getNombre() +
+" " + a.getApellidos() + " " + a.getCurso());
+}
+public Alumno buscarAlumnoPorDNI(String dni) {
+Alumno alumno = null;
+ArrayList<Alumno> listaAlumnos = leerAlumnos();
+for (int i = 0; i < listaAlumnos.size(); i++) {
+if (listaAlumnos.get(i).getDni().equalsIgnoreCase(dni)) {
+alumno = listaAlumnos.get(i);
+}
+}
+return alumno;
+}
+public void altaAlumno(Alumno alumno) {
+ArrayList<Alumno> listaAlumno = leerAlumnos();
+listaAlumno.add(alumno);
+escribirAlumnos(listaAlumno);
+}
+
+
+
+
+}
